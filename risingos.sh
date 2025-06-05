0e454ef91adbd22f7a3cec2fa39a8c2db794f990
procrastinator-dev <murtazakolachi10@gmail.com>
2025-06-05 14:55:31 +0500

    Update risingos.sh

--- risingos.sh
+++ risingos.sh
@@ -2,10 +2,10 @@ rm -rf .repo/local_manifests && \
 rm -rf {device,vendor,kernel,hardware}/xiaomi && \
 repo init --depth=1 --no-repo-verify -u https://github.com/RisingOS-Revived/android -b qpr2 -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
-git clone https://github.com/2faced-sword/device_xiaomi_apollo -b rising device/xiaomi/apollo && \
-git clone https://github.com/2faced-sword/vendor_xiaomi_apollo -b main vendor/xiaomi/apollo && \
-git clone https://github.com/2faced-sword/android_kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
-git clone https://github.com/2faced-sword/hardware_xiaomi -b fifteen hardware/xiaomi && \
+git clone https://github.com/krastinator/android_device_xiaomi_apollo -b rising device/xiaomi/apollo && \
+git clone https://github.com/krastinator/android_vendor_xiaomi_apollo -b main vendor/xiaomi/apollo && \
+git clone https://github.com/krastinator/android_kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
+git clone https://github.com/krastinator/hardware_xiaomi -b fifteen hardware/xiaomi && \
 source build/envsetup.sh && \
 gk -s && \
 riseup apollo user && \
