f20dd1081f6098ab0b2070d16795ed309b5bbac9
procrastinator-dev <murtazakolachi10@gmail.com>
2025-06-13 18:46:52 +0500

    Update CrDroid.sh

--- CrDroid.sh
+++ CrDroid.sh
@@ -4,8 +4,8 @@ rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 15.0 --git-lfs -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
-git clone https://github.com/Krastinator/android_device_xiaomi_apollo -b main device/xiaomi/apollo && \
-git clone https://github.com/Krastinator/android_vendor_xiaomi_apollo -b main vendor/xiaomi/apollo && \
+git clone https://github.com/Krastinator/android_device_xiaomi_apollo -b crdroid device/xiaomi/apollo && \
+git clone https://github.com/Krastinator/android_vendor_xiaomi_apollo -b crdroid vendor/xiaomi/apollo && \
 git clone https://github.com/Krastinator/android_kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
 git clone https://github.com/Krastinator/hardware_xiaomi -b fifteen hardware/xiaomi && \
 . build/envsetup.sh && \
