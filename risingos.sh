a55c5ed8b0026fdc3bbe16afed2b849426230500
procrastinator-dev <murtazakolachi10@gmail.com>
2025-06-16 11:18:12 +0500

    Update risingos.sh

--- risingos.sh
+++ risingos.sh
@@ -1,7 +1,7 @@
 rm -rf .repo/local_manifests && \
 rm -rf {device,vendor,kernel,hardware}/xiaomi && \
 rm -rf vendor/lineage-priv/keys && \
-repo init --depth=1 --no-repo-verify -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs -g default,-mips,-darwin,-notdefault && \
+repo init --depth=1 --no-repo-verify -u https://github.com/RisingOS-Revived/android -b new --git-lfs -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
 git clone https://github.com/krastinator/android_device_xiaomi_apollo -b rising device/xiaomi/apollo && \
 git clone https://github.com/krastinator/android_vendor_xiaomi_apollo -b main vendor/xiaomi/apollo && \
