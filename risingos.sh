cbb9dc7f4316570579be2f2e8bbacbadb727c0eb
procrastinator-dev <murtazakolachi10@gmail.com>
2025-06-16 23:36:48 +0500

    Update risingos.sh

--- risingos.sh
+++ risingos.sh
@@ -9,8 +9,14 @@ git clone https://github.com/krastinator/android_vendor_xiaomi_apollo -b main ve
 git clone https://github.com/krastinator/android_kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
 git clone https://github.com/krastinator/hardware_xiaomi -b fifteen hardware/xiaomi && \
 git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX && \
-git clone https://github.com/Krastinator/keys.git -b ros vendor/lineage-priv/keys && \
 . build/envsetup.sh && \
 riseup apollo user && \
 make installclean && \
 rise b
+rm -rf out/target/product/core && rm -rf out/target/product/gapps; \
+cd out/target/product && mv apollo core && cd ../../..; \
+# Gapps Build
+cd device/xiaomi/apollo && rm lineage_apollo.mk && mv gapps.txt lineage_apollo.mk && cd ../../..; \
+. build/envsetup.sh; \
+riseup apollo user && make installclean && rise b; \
+cd out/target/product && mv apollo gapps && cd ../../..; \
