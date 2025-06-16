df043f30e0ff8a93fd7be13547d08a6bda9bf3e2
procrastinator-dev <murtazakolachi10@gmail.com>
2025-06-16 18:20:48 +0500

    Update risingos.sh

--- risingos.sh
+++ risingos.sh
@@ -1,6 +1,7 @@
 rm -rf .repo/local_manifests && \
 rm -rf {device,vendor,kernel,hardware}/xiaomi && \
 rm -rf vendor/lineage-priv/keys && \
+rm -rf packages/apps/ViPER4AndroidFX && \
 repo init --depth=1 --no-repo-verify -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
 git clone https://github.com/krastinator/android_device_xiaomi_apollo -b rising device/xiaomi/apollo && \
