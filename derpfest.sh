0a8a80c64dd8d80e2223535e02aa06095edbcfea
procrastinator-dev <murtazakolachi10@gmail.com>
2025-07-01 07:22:59 +0500

    Update derpfest.sh

--- derpfest.sh
+++ derpfest.sh
@@ -10,8 +10,5 @@ git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b main kernel/
 git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka hardware/xiaomi && \
 rm -rf packages/apps/ViPER4AndroidFX && \
 git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX && \
-export BUILD_USERNAME=Murtaza; \
-export BUILD_HOSTNAME=crave; \
-export TZ=Asia/Karachi; \
 . build/envsetup.sh && \
 lunch lineage_apollo-bp1a-user && make installclean && mka derp
