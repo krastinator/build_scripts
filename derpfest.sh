0c6f4400f3f6161538586043b9cd46cb672a9979
procrastinator-dev <murtazakolachi10@gmail.com>
2025-07-01 07:22:37 +0500

    Create derpfest.sh

---
+++ derpfest.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+rm -rf .repo/local_manifests; \
+rm -rf {device,vendor,kernel,hardware}/xiaomi; \
+repo init --depth=1 --no-repo-verify -u https://github.com/krastinator/manifest.git -b 15.2 --git-lfs -g default,-mips,-darwin,-notdefault && \
+/opt/crave/resync.sh && \
+git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b derpfest device/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b main vendor/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
+git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka hardware/xiaomi && \
+rm -rf packages/apps/ViPER4AndroidFX && \
+git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX && \
+export BUILD_USERNAME=Murtaza; \
+export BUILD_HOSTNAME=crave; \
+export TZ=Asia/Karachi; \
+. build/envsetup.sh && \
+lunch lineage_apollo-bp1a-user && make installclean && mka derp
