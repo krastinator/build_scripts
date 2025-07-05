6de3272c04fb50e22b79c15818f8612389ab04b4
Kastinator <murtazakolachi10@gmail.com>
2025-07-05 09:25:31 +0500

    Update Voltage.sh

--- Voltage.sh
+++ Voltage.sh
@@ -11,6 +11,8 @@ rm -rf kernel/xiaomi; \
 git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
 rm -rf hardware/xiaomi; \
 git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka hardware/xiaomi && \
+rm -rf hardware/dolby
+git clone https://github.com/MurtazaKolachi/hardware_dolby -b sony-1.3 hardware/dolby && \
 rm -rf packages/apps/ViPER4AndroidFX && \
 git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX && \
 . build/envsetup.sh && \
