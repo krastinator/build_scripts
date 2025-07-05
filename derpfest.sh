85d514dd6d7ad7c73d6a0ff9f7e647d02c97d156
Kastinator <murtazakolachi10@gmail.com>
2025-07-05 17:17:10 +0500

    Update derpfest.sh

--- derpfest.sh
+++ derpfest.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests; \
-rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/krastinator/manifest.git -b 15.2 --git-lfs -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
+rm -rf device/xiaomi; \
 git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b derpfest device/xiaomi/apollo && \
+rm -rf vendor/xiaomi; \
 git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b main vendor/xiaomi/apollo && \
+rm -rf kernel/xiaomi; \
 git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
+rm -rf hardware/xiaomi; \
 git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka hardware/xiaomi && \
+rm -rf hardware/dolby && \
+git clone https://github.com/MurtazaKolachi/hardware_dolby -b sony-1.3 hardware/dolby && \
 rm -rf packages/apps/ViPER4AndroidFX && \
 git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX && \
 . build/envsetup.sh && \
